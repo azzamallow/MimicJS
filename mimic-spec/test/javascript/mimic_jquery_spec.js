672e356f5669c661ff7ee35625608ff442de46f9
azzamallow <azzamallow@gmail.com>
2010-02-06 13:38:41 +1100

    updated error messages for jquery behaviour

--- mimic-spec/test/javascript/mimic_jquery_spec.js
+++ mimic-spec/test/javascript/mimic_jquery_spec.js
@@ -14,7 +14,7 @@ Screw.Unit(function() {
 		it('should fail to assert against selector', function() {
 			given.	jQuery('.inqueued');
 			when.	jQuery().usingSelector('.enqueued');
-			then.	it.should.say('The selector ".enqueued" was expected but was not used!');
+			then.	it.should.say('Your specification did not pass!<br/><p>The selector "<b>.enqueued</b>" was expected but was not used!');
 		});
 		
 		it('should allow assertions of a selector never being used', function() {
@@ -29,7 +29,7 @@ Screw.Unit(function() {
 		it('should fail as the a selector was used when it was expected to not be used', function() {
 			given.	jQuery('.enqueued');
 			when.	jQuery().usingSelector('.enqueued').neverHappens();
-			then.	it.should.say('The selector ".enqueued" was expected to never be used, however it was!');
+			then.	it.should.say('Your specification did not pass!<br/><p>The selector "<b>.enqueued</b>" was expected to never be used, however it was!');
 		});
 		
 		it('should allow for simple assertions', function() {
@@ -40,7 +40,7 @@ Screw.Unit(function() {
 		it('should fail as assertion is wrong', function() {
 			given.	jQuery('.enqueued').show();
 			when.	jQuery().usingSelector('.enqueued').should('show2');
-			then.	it.should.say('The function "show2" was expected but did not get called!');
+			then.	it.should.say('Your specification did not pass!<br/><p>The function <b>show2()</b> was expected but did not get called!');
 		});
 		
 		it('should allow for multiple assertions in one should', function() {
@@ -52,14 +52,14 @@ Screw.Unit(function() {
 		it('should fail when allowing for multiple assertions in one should', function() {
 			given.	jQuery('.enqueued').show().hide();
 			when.	jQuery().usingSelector('.enqueued').should('show').and('hide2');
-			then.	it.should.say('The function "hide2" was expected but did not get called!');
+			then.	it.should.say('Your specification did not pass!<br/><p>The function <b>hide2()</b> was expected but did not get called!');
 		});
 		
 		it('should fail when doing multiple assertions in multiple shoulds', function() {
 			given.	jQuery('.enqueued').show().hide();
 			when.	jQuery().usingSelector('.enqueued').should('show')
 			and.	jQuery().usingSelector('.enqueued').should('hide2');
-			then.	it.should.say('The function "hide2" was expected but did not get called!');
+			then.	it.should.say('Your specification did not pass!<br/><p>The function <b>hide2()</b> was expected but did not get called!');
 		});
 		
 		it('should allow for multiple assersions in multiple shoulds', function() {
@@ -80,7 +80,7 @@ Screw.Unit(function() {
 			and.	jQuery('.enqueued').hide();
 			when.	jQuery().usingSelector('.enqueued').should('show2')
 			and.	jQuery().usingSelector('.enqueued').should('hide');
-			then.	it.should.say('The function "show2" was expected but did not get called!');
+			then.	it.should.say('Your specification did not pass!<br/><p>The function <b>show2()</b> was expected but did not get called!');
 		});
 		
 		it('should allow for an assertion of a function with a parameter', function() {
@@ -91,7 +91,7 @@ Screw.Unit(function() {
 		it('should fail for an assertion of a function with a parameter', function() {
 			given.	jQuery('.enqueued').show('sp33d');
 			when.	jQuery().usingSelector('.enqueued').should('show').using('speed');
-			then.	it.should.say('The function "show" was expected to be called with ("speed") but was called with ("sp33d")');
+			then.	it.should.say('Your specification did not pass!<br/><p>The function <b>show()</b> was expected to be called with <b>("speed")</b> but was called with <b>("sp33d")</b>');
 		});
 		
 		it('should allow for an assertion of multiple functions with parameters', function() {
@@ -102,7 +102,7 @@ Screw.Unit(function() {
 		it('should fail for an assertion of a function with a parameter', function() {
 			given.	jQuery('.enqueued').show().tabs('select', 1);
 			when.	jQuery().usingSelector('.enqueued').should('show').and('tabz').using('select', 1);
-			then.	it.should.say('The function "tabz" was expected but did not get called!');
+			then.	it.should.say('Your specification did not pass!<br/><p>The function <b>tabz()</b> was expected but did not get called!');
 		});
 		
 		it('should allow for an assertion of a function with multiple parameters', function() {
@@ -113,7 +113,7 @@ Screw.Unit(function() {
 		it('should fail for an assertion of a function with a parameter', function() {
 			given.	jQuery('.enqueued').attr({'k3y':'valu3'}, 'theNam3', [1,2,3,'4']);
 			when.	jQuery().usingSelector('.enqueued').should('attr').using({'key':'value'}, 'theName', [1,2,3,'4']);
-			then.	it.should.say('The function "attr" was expected to be called with ({"key": "value"}, "theName", [1, 2, 3, "4"]) but was called with ({"k3y": "valu3"}, "theNam3", [1, 2, 3, "4"])');
+			then.	it.should.say('Your specification did not pass!<br/><p>The function <b>attr()</b> was expected to be called with <b>({"key": "value"}, "theName", [1, 2, 3, "4"])</b> but was called with <b>({"k3y": "valu3"}, "theNam3", [1, 2, 3, "4"])</b>');
 		});
 		
 		it('should allow calls to jquery objects with up to four parameters', function() {
--- src/scripts/jquery/mimic.verify.js
+++ src/scripts/jquery/mimic.verify.js
@@ -7,18 +7,18 @@ Mimic.Verifier.JQuery = function(mimic) {
 		var failedSelector = expectation.hasFailedSelectorFrom(calls);
 		if (failedSelector != false) {
 			if (failedSelector.never == true) {
-				throw('The selector "' + expectation.selector +'" was expected to never be used, however it was!');
+				throw('Your specification did not pass!<br/><p>The selector "<b>' + expectation.selector +'</b>" was expected to never be used, however it was!');
 			} else {
-				throw('The selector "' + expectation.selector +'" was expected but was not used!');
+				throw('Your specification did not pass!<br/><p>The selector "<b>' + expectation.selector +'</b>" was expected but was not used!');
 			}
 		}
 		
 		var failedExpectations = expectation.hasFailedExpectationsFrom(calls);
 		if (failedExpectations.length > 0) {
 			if (failedExpectations[0].call == null) {
-				throw('The function "' + failedExpectations[0].expectation.name + '" was expected but did not get called!');				
+				throw('Your specification did not pass!<br/><p>The function <b>' + failedExpectations[0].expectation.name + '()</b> was expected but did not get called!');				
 			} else if (failedExpectations[0].expectation.name == failedExpectations[0].call.name) { 
-				throw('The function "' + failedExpectations[0].expectation.name + '" was expected to be called with (' + Mimic.Util.Object.toString(failedExpectations[0].expectation.value) + ') but was called with (' + Mimic.Util.Object.toString(failedExpectations[0].call.value) + ')');
+				throw('Your specification did not pass!<br/><p>The function <b>' + failedExpectations[0].expectation.name + '()</b> was expected to be called with <b>(' + Mimic.Util.Object.toString(failedExpectations[0].expectation.value) + ')</b> but was called with <b>(' + Mimic.Util.Object.toString(failedExpectations[0].call.value) + ')</b>');
 			}
 		}
 	}
