678a996a0cec8f48181bf158a0594ffcd5189561
Aaron Triantafyllidis <aarontriantafyllidis@Aaron-Triantafyllidiss-MacBook-Pro.local>
2010-08-19 18:25:48 +1000

    removed jquery implementation

--- src/scripts/jquery/mimic.call.js
+++
@@ -1,9 +0,0 @@
-Mimic.Call.JQuery = function(selector, context, call) {
-	this.selector = selector;
-	this.context = context;
-	this.call = call;
-	
-	this.set = function(name, value) {
-		this.call = {'name': name, 'value': value};
-	};
-};
