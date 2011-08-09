80c11c7f391a8389d314fe9bbcc9463ba6275134
azzamallow <azzamallow@gmail.com>
2011-08-09 14:50:14 +1000

    fixed scope issues with ajax monitor

--- src/scripts/ajax/mimic.ajax.js
+++ src/scripts/ajax/mimic.ajax.js
@@ -3,49 +3,46 @@ if (typeof Mimic === 'undefined') {
 }
 
 Mimic.Ajax = function () {
-	var that = this;
-	this.data = [];
-	var matchers = function (url, ajax) {
+	data = [];
+	var matchers = function (url) {
 		return {
 			toHaveResponse: function (status, text) {
-				ajax.data.push({ 'url': url, 'text': text, 'status': status });
+				data.push({ 'url': url, 'text': text, 'status': status });
 			}
 		};
 	};
 	
 	this.request = function (url) {
-		return matchers(url, that);
+		return matchers(url);
 	};
 	
 	this.start = function () {
-		XMLHttpRequest = function (ajax) {
-			return function() {
-				this.onreadystatechange = undefined;
-				this.readyState = -1;
-				this.responseText = null;
-				this.responseXML = null;
-				this.status = -1;
-				this.statusText = null;
-				this.open = function (method, url, async, user, password) { 
-					for (var i = 0; i< ajax.data.length; i++) {
-						if (ajax.data[i].url === url) {
-							this.responseText = ajax.data[i].text;
-							this.status = ajax.data[i].status;
-							this.readyState = 4;
-						}
+		XMLHttpRequest = function() {
+			this.onreadystatechange = undefined;
+			this.readyState = -1;
+			this.responseText = null;
+			this.responseXML = null;
+			this.status = -1;
+			this.statusText = null;
+			this.open = function (method, url, async, user, password) { 
+				for (var i = 0; i< data.length; i++) {
+					if (data[i].url === url) {
+						this.responseText = data[i].text;
+						this.status = data[i].status;
+						this.readyState = 4;
 					}
-				};
-				this.send = function (object) {
-					if (this.onreadystatechange !== undefined) {
-						this.onreadystatechange(this);
-					}
-				};
-				this.getAllResponseHeaders = function() {};
-				this.getResponseHeader = function(header) {};
-				this.setRequestHeader = function(header, value) {};
-				this.abort = function() {};
+				}
 			};
-		}(that);
+			this.send = function (object) {
+				if (this.onreadystatechange !== undefined) {
+					this.onreadystatechange(this);
+				}
+			};
+			this.getAllResponseHeaders = function() {};
+			this.getResponseHeader = function(header) {};
+			this.setRequestHeader = function(header, value) {};
+			this.abort = function() {};
+		};
 	};
 };
 
--- src/scripts/overrides/mimic.overrides.jasmine.js
+++ src/scripts/overrides/mimic.overrides.jasmine.js
@@ -1,29 +1,27 @@
 if (typeof jasmine !== 'undefined') {
 	var jasmineIt = jasmine.Env.prototype.it;
 	jasmine.Env.prototype.it = function(description, func) {
-		var toReturn = jasmineIt.call(this, description, function() {
+		return jasmineIt.call(this, description, function() {
 			try {
 				ajax = new Mimic.Ajax();
 				ajax.start();
 				func(ajax.request);
 
 		        Mimic.verify();
-		        Mimic.reset();
-		        if (thrown != null) {
-		            var expected = thrown;
-		            thrown = null;
-		            throw('An exception was expected to be thrown but was not. The exception expected is:<br/><br/>' + expected);
-		        }
-		    } catch(e) {
-		        Mimic.reset();
-		        if (thrown != null) {
-		            var expected = thrown;
-		            thrown = null;
-		            expect(e).toEqual(expected);
-		        }
-		    }
+   		        Mimic.reset();
+ 		        if (thrown != null) {
+ 		            var expected = thrown;
+ 		            thrown = null;
+ 		            throw('An exception was expected to be thrown but was not. The exception expected is:<br/><br/>' + expected);
+ 		        }
+ 		    } catch(e) {
+ 		        Mimic.reset();
+ 		        if (thrown != null) {
+ 		            var expected = thrown;
+ 		            thrown = null;
+ 		            expect(e).toEqual(expected);
+ 		        }
+ 		    }
 		});
-		
-		return toReturn;
 	};
 }
